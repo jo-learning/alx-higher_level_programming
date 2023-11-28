6950f6c7eb866fecc577d1d4d90971c0a9526ffc
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-11-28 09:56:06 -0800

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for s in range(ord('a'), ord('z')+1):
-    print(f"{s:c}", end="")
+    print("{:c}".format(s), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for s in range(ord('a'), ord('z')+1):
-    if s != ord('q') and  s != ord('e'):
+    if s != ord('q') and s != ord('e'):
         print("{:c}".format(s), end="")
