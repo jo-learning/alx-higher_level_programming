389ac396c25c2380d9526a6f6fd2f7dc6a0e3457
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-11-30 12:23:39 -0800

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     argcount = len(sys.argv) - 1
     if argcount == 0:
         print("0 arguments.")
-    else:
+    elif argcount == 1:
         print("{:d} argument:".format(argcount))
+    else:
+        print("{:d} arguments:".format(argcount))
     for i in range(argcount):
         print("{:d}: {}".format(i+1, sys.argv[i+1]))
