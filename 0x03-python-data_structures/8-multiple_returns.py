78b47c93a78342c1d22c07bc1dbc49acae10f83c
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-12-03 11:49:44 -0800

    updated

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        sentence[0] = None
-    return (len(sentence), sentence[0])
+    if len(sentence) <= 0:
+        first = None
+    else:
+        first = sentence[0]
+    return (len(sentence), first)
