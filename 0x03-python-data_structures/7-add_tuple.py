83c3724622b27b80bd9f3fa4e1fc5370041ace30
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-12-03 11:12:40 -0800

    updated

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,4 +4,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = tuple_a[1] if len(tuple_a) > 1 else 0
     c = tuple_b[0] if len(tuple_b) > 0 else 0
     d = tuple_b[1] if len(tuple_b) > 1 else 0
-    return(a+c, b+d)
+    return (a+c, b+d)
