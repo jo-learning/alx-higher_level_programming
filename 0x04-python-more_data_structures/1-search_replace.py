3553b39094c02d0a562be47c00c44d3bf410b13e
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-12-06 13:00:11 -0800

    updated

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    if search < len(new_list) and search > -len(new_list):
-        if search > 0:
-            new_list[search-1] = replace
-        else:
-            new_list[search] = replace
-    return new_list
-        
+    return list(map(lambda i: replace if i == search else i, my_list))
