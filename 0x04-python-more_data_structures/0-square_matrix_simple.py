fad07e0532b1edc5408b2a30ac005fe56863fc62
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-12-06 12:42:29 -0800

    updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda submatrix: list(map(lambda i: i**2, submatrix)), matrix))
+    return list(map(lambda submatrix: list(map(lambda i: i ** 2, submatrix)), matrix))
